527941a6c92be4857e219d0ed3280a2c1933f1b5
Apisak Srihamat <36180751+srihamat@users.noreply.github.com>
2020-03-16 20:02:53 +0700

    Initial test02.py

---
+++ test02.py
@@ -0,0 +1,31 @@
+def api_roman(value):
+    strTest = ''
+    if ((value % 10) == 0):
+        strTest = 'X'
+    if (value > 10):
+        strTest += 'X'        
+    if ((value % 5) == 4):
+        strTest += 'I' 
+    if ((value % 9) == 0):
+        strTest += 'X'
+    if ((value % 19) == 0):
+        strTest += 'X'
+    if (((value % 10) >= 5) and ((value % 10) <= 8)):
+        strTest += 'V'
+    if ((value % 5) == 1):
+        strTest += 'I'
+    if ((value % 5) == 2):
+        strTest += 'II'
+    if ((value % 5) == 3):
+        strTest += 'III'
+    print(strTest)
+
+i = 1
+while (i < 20):
+    api_roman(i)
+    i = i + 1
+    
+        
+    
+        
+    
